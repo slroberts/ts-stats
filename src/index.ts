bc31d3957ebc90573aef8327232d012baad8bed1
Shomari Roberts <shomariroberts@gmail.com>
2020-12-21 17:31:57 -0500

    intalled file sync module from node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hi there');
+// remember to npm install @types/node for built in node modules
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8',
+});
