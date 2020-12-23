25ee9a69c59b4f976443fc8500170a6835c35cd3
Shomari Roberts <shomariroberts@gmail.com>
2020-12-23 17:40:39 -0500

    updated file paths in MatchReader

--- build/inheritance/MatchReader.js
+++ build/inheritance/MatchReader.js
@@ -15,7 +15,7 @@ var __extends = (this && this.__extends) || (function () {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MatchReader = void 0;
 var CsvFileReader_1 = require("./CsvFileReader");
-var utils_1 = require("./utils");
+var utils_1 = require("../utils");
 var MatchReader = /** @class */ (function (_super) {
     __extends(MatchReader, _super);
     function MatchReader() {
