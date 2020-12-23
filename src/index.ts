17939816f41d6b081c0272b540d8a8d4a4cad7da
Shomari Roberts <shomariroberts@gmail.com>
2020-12-23 18:49:19 -0500

    refactored index to use newly created components

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
+// Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
 
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-console.log(`Man United won ${manUnitedWins} games.`);
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
